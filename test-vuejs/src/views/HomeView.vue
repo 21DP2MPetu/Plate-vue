8dfe18f7c6c42e59cfa6a5c6778867e4e90ed837
21DP2MPetu <106648971+21DP2MPetu@users.noreply.github.com>
2023-09-29 10:24:19 +0000

    small changes (just fixes)

--- test-vuejs/src/views/HomeView.vue
+++ test-vuejs/src/views/HomeView.vue
@@ -2,7 +2,7 @@
     <div class="container">
       <h1 contenteditable>The Real Durak</h1>
       <p>The game with crazy experience!</p>
-      <img src="" alt="durak">
+      <img class="main_image" src="../assets/images/durak_image.png" alt="durak">
     </div>
     </template>
     
