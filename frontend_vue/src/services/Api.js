6a518ebe9fcb4bd4389dd06b0e2bfe1f3fd7a161
21DP2MPetu <106648971+21DP2MPetu@users.noreply.github.com>
2023-11-26 16:02:13 +0000

    FrontEnd + BackEnd

--- frontend_vue/src/services/Api.js
+++ frontend_vue/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: `https://probable-invention-wp7qjrjwg97296j7-5173.app.github.dev/`
+        baseURL: `http://localhost:8081//`
     })
 }
--- frontend_vue/src/views/SignUpView.vue
+++ frontend_vue/src/views/SignUpView.vue
@@ -1,74 +1,95 @@
-<script setup>
-    var email;
-    var username;
-    var password;
+<script>
+    // var email;
+    // var username;
+    // var password;
 
-    function SignUp() {
-        console.log(email);
-        console.log(username);
-        console.log(password);
-    }
+    // function SignUp() {
+    //     console.log(email);
+    //     console.log(username);
+    //     console.log(password);
+    // }
 
-    function Pass() {
-        if (CheckPass() == true) {
-            var preal = document.getElementById("password");
-            preal.classList.add("correct");
-            preal.classList.remove("wrong");
-        } else {
-            var preal = document.getElementById("password");
-            preal.classList.add("wrong");
-            preal.classList.remove("correct");
-        }
-    }
+    // function Pass() {
+    //     if (CheckPass() == true) {
+    //         var preal = document.getElementById("password");
+    //         preal.classList.add("correct");
+    //         preal.classList.remove("wrong");
+    //     } else {
+    //         var preal = document.getElementById("password");
+    //         preal.classList.add("wrong");
+    //         preal.classList.remove("correct");
+    //     }
+    // }
 
-    function Username() {
-        if (CheckUsername() == true) {
-            var ureal = document.getElementById("username");
-            ureal.classList.add("correct");
-            ureal.classList.remove("wrong");
-        } else {
-            var ureal = document.getElementById("username");
-            ureal.classList.add("wrong");
-            ureal.classList.remove("correct");
-        }
-    }
+    // function Username() {
+    //     if (CheckUsername() == true) {
+    //         var ureal = document.getElementById("username");
+    //         ureal.classList.add("correct");
+    //         ureal.classList.remove("wrong");
+    //     } else {
+    //         var ureal = document.getElementById("username");
+    //         ureal.classList.add("wrong");
+    //         ureal.classList.remove("correct");
+    //     }
+    // }
     
-    function Email() {
-        if (CheckEmail() == true) {
-            var ereal = document.getElementById("email");
-            ereal.classList.add("correct");
-            ereal.classList.remove("wrong");
-        } else {
-            var ereal = document.getElementById("email");
-            ereal.classList.add("wrong");
-            ereal.classList.remove("correct");
+    // function Email() {
+    //     if (CheckEmail() == true) {
+    //         var ereal = document.getElementById("email");
+    //         ereal.classList.add("correct");
+    //         ereal.classList.remove("wrong");
+    //     } else {
+    //         var ereal = document.getElementById("email");
+    //         ereal.classList.add("wrong");
+    //         ereal.classList.remove("correct");
+    //     }
+    // }
+
+    // function CheckPass() {
+    //     if (password.length < 8) {
+    //         return false;
+    //     } else {
+    //         return true;
+    //     }
+    // }
+
+    // function CheckUsername() {
+    //     if (username.length < 4) {
+    //         return false;
+    //     } else {
+    //         return true;
+    //     }
+    // }
+
+    // function CheckEmail() {
+    //     if (!email.includes('@') || !email.includes('.')) {
+    //         return false;
+    //     } else {
+    //         return true;
+    //     }
+    // }
+
+import AuthentificationServices from '@/services/AuthentificationServices.js'
+
+export default {
+    data () {
+        return {
+            'email': 'abc',
+            'username': 'abs',
+            'password': '123456',
+
+        }
+    },
+    methods: {
+        SignUp () {
+            AuthentificationServices.register({
+                email: this.email,
+                username: this.username,
+                password: this.password
+            })
         }
     }
-
-    function CheckPass() {
-        if (password.length < 8) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function CheckUsername() {
-        if (username.length < 4) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function CheckEmail() {
-        if (!email.includes('@') || !email.includes('.')) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
+}
 
 </script>
 
